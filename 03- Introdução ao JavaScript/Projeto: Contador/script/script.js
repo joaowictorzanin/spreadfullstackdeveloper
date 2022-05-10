c1858d791b9a41216cc9ca1d99656b7a202745fe
joao.zanin <103003839+joaowictorzanin@users.noreply.github.com>
2022-05-09 22:40:22 -0300

    09.05

---
+++ 03- Introdução ao JavaScript/Projeto: Contador/script/script.js
@@ -0,0 +1,13 @@
+var count = 0 
+
+const currentNumber = document.getElementById('currentNumber')
+
+function increment(){
+    count++
+    currentNumber.innerHTML = count
+}
+
+function decrement(){
+    count--
+    currentNumber.innerHTML = count
+}
